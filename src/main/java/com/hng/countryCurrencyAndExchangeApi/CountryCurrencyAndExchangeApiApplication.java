b5180bf697e8b686c05121798d2c4db69b4b608e
Ayomide <oyetimehin31@gmail.com>
2025-10-25 14:22:53 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/hng/countryCurrencyAndExchangeApi/CountryCurrencyAndExchangeApiApplication.java
@@ -0,0 +1,13 @@
+package com.hng.countryCurrencyAndExchangeApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCurrencyAndExchangeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryCurrencyAndExchangeApiApplication.class, args);
+	}
+
+}
